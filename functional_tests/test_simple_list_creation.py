a5886a8e9ceedf43570b873732bf32752b14dfb6
HorridTom <woodcock.thomas@gmail.com>
2017-12-25 19:02:33 +0000

    rename all item input ids and names. still broken.

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -16,7 +16,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -33,7 +33,7 @@ class NewVisitorTest(FunctionalTest):
         # There is still a text box inviting him to add another item. He
         # enters "Drink Duff" (Homer is looking forward to this bit)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Drink Duff')
         inputbox.send_keys(Keys.ENTER)
 
@@ -48,7 +48,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Homer starts a new todo list
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy Duff')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Duff')
@@ -71,7 +71,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('Drink Duff', page_text)
 
         # Bart starts a new list by entering a new item
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Cause mischief')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Cause mischief')
